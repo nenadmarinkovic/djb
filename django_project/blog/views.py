f7e78123a91f31088307e1a83345053d21b0be9e
Nenad Marinković <nenadmarinkovic@protonmail.com>
2022-03-11 09:57:01 +0100

    blog posts dummy data

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,10 +1,36 @@
+from turtle import pos, title
+from django import views
 from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
 
+posts = [
+    {
+        'author': 'Nenad',
+        'title': 'Post 1',
+        'content': 'Content 1 Blog post',
+        'date_posted': '11.3.2022'
+    },
+     {
+        'author': 'Helena',
+        'title': 'Post 2',
+        'content': 'Content 2 Blog post',
+        'date_posted': '10.3.2022'
+    },
+      {
+        'author': 'Xxx',
+        'title': 'Post 3',
+        'content': 'Content 3 Blog post',
+        'date_posted': '11.3.2022'
+    }
+]
+
 def home(request):
-    return HttpResponse('<h1> Homepage </h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return HttpResponse('<h1> About </h1>')
+    return render(request, 'blog/about.html', {'title':'About'})
