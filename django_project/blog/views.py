ada50077d10d7f02fdcb68640243fe9462992e4d
Nenad Marinković <nenadmarinkovic@protonmail.com>
2022-03-14 15:27:10 +0100

    users added

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -2,33 +2,13 @@ from turtle import pos, title
 from django import views
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 
 # Create your views here.
 
-posts = [
-    {
-        'author': 'Nenad',
-        'title': 'Post 1',
-        'content': 'Content 1 Blog post',
-        'date_posted': '11.3.2022'
-    },
-     {
-        'author': 'Helena',
-        'title': 'Post 2',
-        'content': 'Content 2 Blog post',
-        'date_posted': '10.3.2022'
-    },
-      {
-        'author': 'Xxx',
-        'title': 'Post 3',
-        'content': 'Content 3 Blog post',
-        'date_posted': '11.3.2022'
-    }
-]
-
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
